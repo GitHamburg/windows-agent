8c7b98684ea2094a090675b873829c36ae82e78d
jsonsean <yangdoudou@foundersc.com>
2019-12-01 11:08:04 +0800

    引用更新

--- cron/plugin.go
+++ cron/plugin.go
@@ -17,7 +17,7 @@ package cron
 import (
 	"github.com/open-falcon/falcon-plus/common/model"
 	"github.com/open-falcon/falcon-plus/modules/agent/g"
-	"github.com/open-falcon/falcon-plus/modules/agent/plugins"
+	"github.com/GitHamburg/windows-agent/plugins"
 	//"github.com/toolkits/file"
 	"log"
 	"os"
